8af2c6bf5a4efb74b4ee09a3bc75e7872d5621d7
Samuel <brayan.cunduri@epn.edu.ec>
2023-06-11 23:16:19 -0500

    de android studio, boton

---
+++ 01 - Android/app/src/main/java/com/example/movilescomp2023a/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.movilescomp2023a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) { // ewscribimos la funcion
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /*fun irActividad(
+        clase: Class<*,
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent);
+    }*/
+
+}
