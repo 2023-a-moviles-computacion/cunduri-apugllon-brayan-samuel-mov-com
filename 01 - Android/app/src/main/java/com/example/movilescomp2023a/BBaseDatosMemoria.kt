9dd3cdab81f5b3cec862422de19f5d7aa8fe0d68
Samuel <brayan.cunduri@epn.edu.ec>
2023-06-26 11:27:10 -0500

    Clase15

--- 01 - Android/app/src/main/java/com/example/movilescomp2023a/BBaseDatosMemoria.kt
+++ 01 - Android/app/src/main/java/com/example/movilescomp2023a/BBaseDatosMemoria.kt
@@ -2,17 +2,17 @@ package com.example.movilescomp2023a
 
 class BBaseDatosMemoria {
     companion object{
-        val arrgloBEntrenador= arrayListOf<BEntrenador>()
+        val arregloBEntrenador= arrayListOf<BEntrenador>()
         init {
-            arrgloBEntrenador
+            arregloBEntrenador
                 .add(
                     BEntrenador(1, "Samuel","a@a.com")
                 )
-            arrgloBEntrenador
+            arregloBEntrenador
                 .add(
                     BEntrenador(2, "Brayan","n@n.com")
                 )
-            arrgloBEntrenador
+            arregloBEntrenador
                 .add(
                     BEntrenador(3, "Hernan","b@b.com")
                 )
--- 01 - Android/app/src/main/java/com/example/movilescomp2023a/BListView.kt
+++ 01 - Android/app/src/main/java/com/example/movilescomp2023a/BListView.kt
@@ -1,11 +1,120 @@
 package com.example.movilescomp2023a
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+
 
 class BListView : AppCompatActivity() {
+
+    val arreglo : ArrayList<BEntrenador> = BBaseDatosMemoria.arregloBEntrenador;
+    var idItemSeleccionado = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blist_view)
+
+        val listView = findViewById<ListView>(R.id.lv_list_view)
+        val adaptador = ArrayAdapter(
+            this, // Contexto
+            android.R.layout.simple_list_item_1, // como se va a ver (XML)
+            arreglo
+        )
+        listView.adapter = adaptador
+        adaptador.notifyDataSetChanged()
+
+        val botonAnadirListView =findViewById<Button>(R.id.btn_anadir_list_view)
+        botonAnadirListView
+            .setOnClickListener {
+                anadirEntrenador(adaptador)
+            }
+        registerForContextMenu(listView)
     }
+
+    fun anadirEntrenador(
+        adaptador: ArrayAdapter<BEntrenador>
+    ){
+        arreglo.add(
+            BEntrenador(1,
+                "Samuel",
+                "Descripcion"
+            )
+        )
+        adaptador.notifyDataSetChanged()
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        //lamamos las opciones del menú
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu,menu)
+        //obtener el id del arraylist seleccionado
+        val info = menuInfo as AdapterView.AdapterContextMenuInfo
+        val id = info.position
+        idItemSeleccionado = id
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.mi_editar->{
+                "${idItemSeleccionado}"
+                return true
+            }
+            R.id.mi_eliminar->{
+                "${idItemSeleccionado}"
+                abrirDialogo()
+                return true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+    fun abrirDialogo(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Desea eliminar?")
+        builder.setPositiveButton(
+            "Aceptar",
+            DialogInterface.OnClickListener{
+                    dialog, which ->
+                // Al aceptar eliminar el registro
+            }
+        )
+        builder.setNegativeButton(
+            "Cancelar",
+            null
+        )
+
+        val opciones= resources.getStringArray(
+            R.array.string_array_opciones_dialogo
+        )
+        val seleccionPrevia= booleanArrayOf(
+            true, // Lunes Seleccionado
+            false, // Martes NO Seleccionado
+            false // // Miercoles NO Seleccionado
+        )
+        builder.setMultiChoiceItems(
+            opciones,
+            seleccionPrevia,
+            { dialog,
+              which,
+              isChecked ->
+                "Dio clic en el item ${which}"
+            }
+        )
+        val dialogo = builder.create()
+        dialogo.show()
+    }
+
+
 }
